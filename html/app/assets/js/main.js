1d333ae58218748b2530b2c1bfe4c3d77073580f
Muskan B <muskan@bluetext.com>
2019-06-24 19:28:27 +0530

    inner page cahnges added

--- html/app/assets/js/main.js
+++ html/app/assets/js/main.js
@@ -3,5 +3,32 @@
 	var height = $('h1.start-text').innerHeight();
 	$('.content.secoend').css('padding-top', height);
 	$('.content.last').css('padding-top', height);
+	  // Banner slider
+	  $('.slider').slick({
+	  	dots: true,
+	  	infinite: true,
+	  	speed: 1500,
+	  	autoplay: true,
+	  	arrows: true,
+	  	prevArrow: '<div class="prev"><span>previous</span></div>',
+	  	nextArrow: '<div class="next"><span>Next</span></div>',
+	  	slidesToShow: 1.1,
+	  	slidesToScroll: 1,
+	  	responsive: [{
+	  		breakpoint: 993,
+	  		settings: {
+	  			arrows: true,
+	  			dots: true,
+	  		}
+	  	},
+	  	{
+	  		breakpoint: 768,
+	  		settings: {
+	  			arrows: false,
+	  			dots: true,
+	  			slidesToShow: 1,
+	  		}
+	  	} ]
+	  });
 
-})(jQuery);
+	})(jQuery);
