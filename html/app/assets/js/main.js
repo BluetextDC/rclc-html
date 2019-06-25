eb3cae998c3e5ad5422059f4f06dd216f3556d68
Muskan B <muskan@bluetext.com>
2019-06-25 12:36:18 +0530

    generic page html

--- html/app/assets/js/main.js
+++ html/app/assets/js/main.js
@@ -1,34 +1,43 @@
 (function($) {
 
-	var height = $('h1.start-text').innerHeight();
-	$('.content.secoend').css('padding-top', height);
-	$('.content.last').css('padding-top', height);
-	  // Banner slider
-	  $('.slider').slick({
-	  	dots: true,
-	  	infinite: true,
-	  	speed: 1500,
-	  	autoplay: true,
-	  	arrows: true,
-	  	prevArrow: '<div class="prev"><span>previous</span></div>',
-	  	nextArrow: '<div class="next"><span>Next</span></div>',
-	  	slidesToShow: 1.1,
-	  	slidesToScroll: 1,
-	  	responsive: [{
-	  		breakpoint: 993,
-	  		settings: {
-	  			arrows: true,
-	  			dots: true,
-	  		}
-	  	},
-	  	{
-	  		breakpoint: 768,
-	  		settings: {
-	  			arrows: false,
-	  			dots: true,
-	  			slidesToShow: 1,
-	  		}
-	  	} ]
-	  });
+  // Banner slider
+  $('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 1500,
+    autoplay: true,
+    arrows: true,
+    prevArrow: '<div class="prev"><span>previous</span></div>',
+    nextArrow: '<div class="next"><span>Next</span></div>',
+    slidesToShow: 1.1,
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint: 993,
+      settings: {
+        arrows: true,
+        dots: true,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: 'unslick'
+    }
+    ]
+  });
+  
+  // refresh slider on browser resize
+  // $(window).resize(function() {
+  //   $('.slider').slick('refresh');
+  // });
 
-	})(jQuery);
+  // Form field float js
+  $(".form-control").focus(function() {
+    $(this).parent().addClass("label-float");
+  }).blur(function() {
+    var inputValue = $(this).val();
+    if (inputValue.length == 0) {
+      $(this).parent().removeClass("label-float");
+    }
+  });
+
+})(jQuery);
