3c513311c77ab6b3c0353c3a2e07799e3024883d
Muskan B <muskan@bluetext.com>
2019-07-04 12:57:27 +0530

    changes added

--- html/app/assets/js/main.js
+++ html/app/assets/js/main.js
@@ -124,10 +124,12 @@
          // caltegory selected hide show elements
         if($checkboxes.filter(':checked').length == 1){
           $(this).parent().parent().parent().parent().find('.first-text').hide();
+          $(this).parent().parent().parent().parent().find('.counter').show();
           $(this).parent().parent().parent().parent().find('.selected-text').show();
           $(this).parent().parent().parent().parent().find('.selected-text').text('item selected');
         }else if($checkboxes.filter(':checked').length >= 2){
           $(this).parent().parent().parent().parent().find('.first-text').hide();
+          $(this).parent().parent().parent().parent().find('.counter').show();
           $(this).parent().parent().parent().parent().find('.selected-text').show();
           $(this).parent().parent().parent().parent().find('.selected-text').text('items selected');
         }else{
