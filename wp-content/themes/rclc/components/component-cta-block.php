0d742898d9b804fef5960dfca0070dc1a8043fb1
Muskan B <muskan@bluetext.com>
2019-07-04 17:50:19 +0530

    changes added

--- wp-content/themes/rclc/components/component-cta-block.php
+++ wp-content/themes/rclc/components/component-cta-block.php
@@ -15,56 +15,9 @@
 					<?php if(get_sub_field('form_id')): ?>
 						<?php the_sub_field('form_id'); ?>
 					<?php endif; ?>
-			<!-- 	<form action="">
-					<div class="row">
-						<div class="col-sm-6">
-							<div class="side-spacer">
-								<div class="field-outer">
-									<label for="firstname">First Name <span class="required">*</span></label>
-									<input type="text" id="firstname" name="firstname" class="form-control">
-								</div>
-							</div>
-						</div>
-						<div class="col-sm-6">
-							<div class="side-spacer">
-								<div class="field-outer">
-									<label for="lastname">Last Name<span class="required">*</span></label>
-									<input type="text" id="lastname" name="lastname" class="form-control">
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-sm-6">
-							<div class="side-spacer">
-								<div class="field-outer">
-									<label for="email">Email <span class="required">*</span></label>
-									<input type="email" id="email" name="email" class="form-control">
-								</div>
-							</div>
-						</div>
-						<div class="col-sm-6">
-							<div class="side-spacer">
-								<div class="field-outer">
-									<label for="message">Message</label>
-									<textarea name="message" id="message" class="form-control"></textarea>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-sm-12">
-							<div class="side-spacer text-right">
-								<div class="submit-outer">
-									<input type="submit" value="submit" class="btn submit-btn">
-								</div>
-							</div>
-						</div>
-					</div>
-				</form> -->
+				</div>
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
 <?php endif; ?>
 <!-- Cta Section -->
