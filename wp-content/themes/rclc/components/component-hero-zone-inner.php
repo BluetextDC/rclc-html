40f79e00e6f7a5126b9cf00fe9d2c4559e1e5eaf
Muskan B <muskan@bluetext.com>
2019-07-15 11:05:25 +0530

    course listing and generic landing pages hero zone changes added

--- wp-content/themes/rclc/components/component-hero-zone-inner.php
+++ wp-content/themes/rclc/components/component-hero-zone-inner.php
@@ -1,18 +1,18 @@
 <?php if( have_rows('hero_zone') ): 
   while( have_rows('hero_zone') ): the_row();?>
     <section class="hero-zone generic-landing">
-      <?php if ( has_post_thumbnail() ) {?>
-        <div class="bg-img" style="background-image: url('<?php  the_post_thumbnail_url(); ?>');"></div>
-      <?php } ?>
+      <?php if(get_sub_field('hero_image')): ?>
+        <div class="bg-img" style="background-image: url('<?php  the_sub_field('hero_image'); ?>');"></div>
+      <?php endif; ?>
       <div class="container">
         <div class="row">
-          <?php if(get_sub_field('content')): ?>
+          <?php if(get_sub_field('hero_text')): ?>
             <div class="content">
-              <h1><?php the_sub_field('content'); ?></h1>
+              <h1><?php the_sub_field('hero_text'); ?></h1>
             </div>
           <?php endif; ?>
         </div>
       </div>
     </section>
   <?php endwhile; ?>  
-<?php endif; ?>
+  <?php endif; ?>
