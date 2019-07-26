694d633fcb87c8cae39c31a6ace593556dc9bad2
Muskan B <muskan@bluetext.com>
2019-07-26 18:30:36 +0530

    three-column-full-width -  radio option for page and post

--- wp-content/themes/rclc/components/component-three-column-full-width-cta.php
+++ wp-content/themes/rclc/components/component-three-column-full-width-cta.php
@@ -3,27 +3,51 @@
   <section class="banner-cta relative">
     <div class="bg-img" style="background-image: url('<?php the_sub_field('background_image');?>');"></div>
     <div class="container">
-      <?php if (have_rows('cta_listing')): ?>
+
+      <!-- Select Pages -->
+      <?php if(get_sub_field('cta_content_display_mode') == 'select_pages'){?>
+        <?php $posts = get_sub_field('select_pages');
+        if( $posts ): ?>
+          <div class="row flex-container padding-lg">
+            <?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
+              <?php setup_postdata($post); ?>
+              <div class="col-md-4">
+                <div class="content">
+                    <?php $texonomy_terms = get_the_terms($post->ID, 'content_type');?>
+                    <?php if($texonomy_terms){ ?>
+                    <?php } else { ?>
+                      <span class="category">Page</span>
+                    <?php }?>
+                    <h3> <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                </div>
+              </div>
+            <?php endforeach; ?>
+          </div>
+          <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+        <?php endif; ?>
+      <?php }?>
+      <!-- Select Pages -->
+
+    <!-- Select Posts -->
+    <?php if(get_sub_field('cta_content_display_mode') == 'select_post'){?>
+      <?php $posts = get_sub_field('select_post');
+      if( $posts ): ?>
         <div class="row flex-container padding-lg">
-         <?php while (have_rows('cta_listing')): the_row();?>
-          <?php if (get_row_layout() == 'item'): ?>
-           <?php $link = get_sub_field('cta');
-           if( $link ): 
-            $link_url = $link['url'];
-            $link_title = $link['title'];
-            $link_target = $link['target'] ? $link['target'] : '_self';
-            ?>
+          <?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
+            <?php setup_postdata($post); ?>
             <div class="col-md-4">
               <div class="content">
-                <h3><a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a></h3>
+                  <h3> <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
               </div>
             </div>
-          <?php endif; ?>
-        <?php endif; ?>
-      <?php endwhile;?>
-    </div>
-  <?php endif; ?>
-</div>
+          <?php endforeach; ?>
+        </div>
+        <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+      <?php endif; ?>
+    <?php }?>
+    <!-- Select Posts -->
+
+  </div>
 </section>
 <?php endif; ?>
 <!-- banner-cta section -->
