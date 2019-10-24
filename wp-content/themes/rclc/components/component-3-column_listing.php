d296e264880579a29e5bcd535f8b709c70fcce81
Muskan B <muskan@bluetext.com>
2019-10-24 17:16:55 +0530

    RCLC - 165-category-title-on-three-column-listing 2

--- wp-content/themes/rclc/components/component-3-column_listing.php
+++ wp-content/themes/rclc/components/component-3-column_listing.php
@@ -164,12 +164,15 @@ promo cards -->
                               <a href="<?php the_permalink(); ?>" class="outer_link"></a>
                               <div class="inner">
                                 <!-- <span class="category">Blogs</span> -->
+                                <span class="category">
                                 <?php 
-                                  foreach((get_the_category()) as $category){
-                                      echo $category->name."<br>";
-                                      echo category_description($category);
+                                     $cat = [];
+                                     foreach((get_the_category()) as $category){
+                                       $cat[] = $category->name;
                                       }
+                                      echo implode (', ',$cat); 
                                   ?>
+                                  </span>
                                 <h3><?php the_title(); ?></h3>
                                 <div class="btn-outer">
                                   <a href="<?php the_permalink(); ?>" class="btn link-btn">all industries</a>
